064eb6f1cd9fadf873f825f6e9b0cc835beb2a4d
Carlos <carlos96831@gmail.com>
2022-01-12 02:09:22 -0800

    added react-typical, a loop, with descriptions

--- my-app/src/PortfolioContainer/Home/Profile.js
+++ my-app/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typical from 'react-typical'
 
 export default function Profile() {
     return (
@@ -7,10 +8,13 @@ export default function Profile() {
             <div className='profile-details'>
                 <div className='profile-colz'>
                     <a href='#'>
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href='#'>
                         <i className='fa fa-facebook-square'></i>
                     </a>
                     <a href='#'>
-                        <i className='fa fa-google-plus-square'></i>
+                        <i className='fa fa-twitter-square'></i>
                     </a>
                     <a href='#'>
                         <i className='fa fa-youtube-square'></i>
@@ -21,6 +25,37 @@ export default function Profile() {
                     <a href='https://github.com/carvasquez206'>
                         <i className='fa fa-github-square'></i>
                     </a>
+                </div>
+
+                <div className="profile-details-name">
+                    <span className="primary-text">
+                        {" "}
+                        Hello I'm <span className="highlighted-text">Carlos</span>
+                    </span>
+                </div>
+                <div className='profile-details-role'>
+                    <span className='primary-text'>
+                        {" "}   
+                        <h1>
+                            {" "} 
+                            <Typical
+                            loop={Infinity} 
+                            steps={[
+                                "Ethusiastic Dev",
+                                1000,
+                                "Full Stack Developer",
+                                1000,
+                                "Mern Stack Dev",
+                                1000,
+                                "Cross Platform Dev",
+                                1000,
+                                "React/React Native Dev",
+                                1000,
+                            ]}
+                        />
+                        </h1>
+                    </span>
+
                 </div>
             </div> 
         </div>
